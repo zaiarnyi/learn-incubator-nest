87893e2c53fcb6584a3a7786de769f9964d758d1
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-03-19 20:43:45 +0200

    feature: ci/cd

--- src/infrastructure/rest/http-exception.filter.ts
+++ src/infrastructure/rest/http-exception.filter.ts
@@ -1,13 +1,18 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, Logger } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  logger = new Logger(HttpExceptionFilter.name);
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+
+    if (status === 422) {
+      this.logger.error(`${exception.message}. ${JSON.stringify(request.body, null, 2)}`);
+    }
 
     // response.status(status).json({
     //   statusCode: status,
