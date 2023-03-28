3ad5602b61a982398f4632377ed53177a02dc4b5
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-03-28 20:12:55 +0300

    feature: finish part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongoConfigDatabase } from './infrastructure/database/configs/databases/mongo-config.database';
 import { UsersModule } from './infrastructure/ioc/users.module';
-// import { PostsModule } from './infrastructure/ioc/posts.module';
-// import { BlogsModule } from './infrastructure/ioc/blogs.module';
-// import { CommentsModule } from './infrastructure/ioc/comments.module';
-// import { TestController } from './presentation/controllers/test.controller';
+import { PostsModule } from './infrastructure/ioc/posts.module';
+import { BlogsModule } from './infrastructure/ioc/blogs.module';
+import { CommentsModule } from './infrastructure/ioc/comments.module';
+import { TestController } from './presentation/controllers/test.controller';
 import { CacheService } from './infrastructure/cache';
 // import { APP_FILTER } from '@nestjs/core';
 // import { HttpExceptionFilter } from './infrastructure/rest/http-exception.filter';
-// import { AuthModule } from './infrastructure/ioc/auth.module';
+import { AuthModule } from './infrastructure/ioc/auth.module';
 
 @Module({
   imports: [
@@ -22,12 +22,12 @@ import { CacheService } from './infrastructure/cache';
       useClass: CacheService,
     }),
     UsersModule,
-    // PostsModule,
-    // BlogsModule,
-    // CommentsModule,
-    // AuthModule,
+    PostsModule,
+    BlogsModule,
+    CommentsModule,
+    AuthModule,
   ],
-  controllers: [],
+  controllers: [TestController],
   // providers: [
   //   {
   //     provide: APP_FILTER,
