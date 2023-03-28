6c849749ddf1b2843eeb99bb44714e7c36af2cde
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-03-28 20:59:47 +0300

    feature: finish part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,38 @@
-import { Module } from '@nestjs/common';
-// import { ConfigModule } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { MongoConfigDatabase } from './infrastructure/database/configs/databases/mongo-config.database';
-// import { UsersModule } from './infrastructure/ioc/users.module';
-// import { PostsModule } from './infrastructure/ioc/posts.module';
-// import { BlogsModule } from './infrastructure/ioc/blogs.module';
-// import { CommentsModule } from './infrastructure/ioc/comments.module';
+import { CacheModule, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoConfigDatabase } from './infrastructure/database/configs/databases/mongo-config.database';
+import { UsersModule } from './infrastructure/ioc/users.module';
+import { PostsModule } from './infrastructure/ioc/posts.module';
+import { BlogsModule } from './infrastructure/ioc/blogs.module';
+import { CommentsModule } from './infrastructure/ioc/comments.module';
 import { TestController } from './presentation/controllers/test.controller';
-// import { CacheService } from './infrastructure/cache';
-// import { APP_FILTER } from '@nestjs/core';
-// import { HttpExceptionFilter } from './infrastructure/rest/http-exception.filter';
-// import { AuthModule } from './infrastructure/ioc/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { CacheService } from './infrastructure/cache';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './infrastructure/rest/http-exception.filter';
+import { AuthModule } from './infrastructure/ioc/auth.module';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({ isGlobal: true }),
-    // MongooseModule.forRootAsync({
-    //   useClass: MongoConfigDatabase,
-    // }),
-    // CacheModule.registerAsync({
-    //   useClass: CacheService,
-    // }),
-    // UsersModule,
-    // PostsModule,
-    // BlogsModule,
-    // CommentsModule,
-    // AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useClass: MongoConfigDatabase,
+    }),
+    CacheModule.registerAsync({
+      useClass: CacheService,
+    }),
+    UsersModule,
+    PostsModule,
+    BlogsModule,
+    CommentsModule,
+    AuthModule,
   ],
   controllers: [TestController],
-  // providers: [
-  //   {
-  //     provide: APP_FILTER,
-  //     useClass: HttpExceptionFilter,
-  //   },
-  // ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,58 @@
 import { NestFactory } from '@nestjs/core';
 import { BadRequestException, HttpStatus, Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
-// import helmet from 'helmet';
-// import bodyParser from 'body-parser';
-// import cookieParser from 'cookie-parser';
-// // import * as compression from 'compression';
-// import rateLimit from 'express-rate-limit';
-// import { HttpExceptionFilter } from './infrastructure/rest/http-exception.filter';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+// import * as compression from 'compression';
+import rateLimit from 'express-rate-limit';
+import { HttpExceptionFilter } from './infrastructure/rest/http-exception.filter';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    // const PORT = +configService.get('PORT', '3005');
-    // Logger.warn(`PORT - ${PORT}`, 'Bootstrap');
+    const configService = app.get(ConfigService);
+    const PORT = +configService.get('PORT', '3005');
+    Logger.warn(`PORT - ${PORT}`, 'Bootstrap');
 
     app.enableCors();
-    // app.use(helmet());
-    // // app.use(compression());
-    // app.use(cookieParser());
-    // app.use(bodyParser.json({ limit: '50mb' }));
-    // app.use(
-    //   bodyParser.urlencoded({
-    //     limit: '50mb',
-    //     extended: true,
-    //     parameterLimit: 50000,
-    //   }),
-    // );
-    // app.use(
-    //   rateLimit({
-    //     windowMs: 1000 * 60 * 60,
-    //     max: 1000, // 1000 requests per windowMs
-    //     message: '⚠️ Too many request created from this IP, please try again after an hour',
-    //   }),
-    // );
-    // app.useGlobalFilters(new HttpExceptionFilter());
-    // app.useGlobalPipes(
-    //   new ValidationPipe({
-    //     transform: true,
-    //     stopAtFirstError: true,
-    //     errorHttpStatusCode: HttpStatus.BAD_REQUEST,
-    //     transformOptions: { enableImplicitConversion: true },
-    //     exceptionFactory: (errors) => {
-    //       const err = errors.map((item) => ({
-    //         message: Object.values(item.constraints)[0],
-    //         field: item.property,
-    //       }));
-    //       throw new BadRequestException(JSON.stringify(err));
-    //     },
-    //   }),
-    // );
-    await app.listen(3005).then((res) => {
-      Logger.log(res, 'Bootstrap');
-    });
-    Logger.log(`Server is listening on port ${3005}`, 'Bootstrap');
+    app.use(helmet());
+    // app.use(compression());
+    app.use(cookieParser());
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(
+      bodyParser.urlencoded({
+        limit: '50mb',
+        extended: true,
+        parameterLimit: 50000,
+      }),
+    );
+    app.use(
+      rateLimit({
+        windowMs: 1000 * 60 * 60,
+        max: 1000, // 1000 requests per windowMs
+        message: '⚠️ Too many request created from this IP, please try again after an hour',
+      }),
+    );
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        stopAtFirstError: true,
+        errorHttpStatusCode: HttpStatus.BAD_REQUEST,
+        transformOptions: { enableImplicitConversion: true },
+        exceptionFactory: (errors) => {
+          const err = errors.map((item) => ({
+            message: Object.values(item.constraints)[0],
+            field: item.property,
+          }));
+          throw new BadRequestException(JSON.stringify(err));
+        },
+      }),
+    );
+    await app.listen(PORT);
+    Logger.log(`Server is listening on port ${PORT}`, 'Bootstrap');
   } catch (e) {
     Logger.error(`❌ Error starting server, ${e}`, '', 'Bootstrap', false);
   }
