789e7f037a232bd1228c21a348cc9f3cd1e057ea
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-03-28 20:26:19 +0300

    feature: finish part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,33 @@
-import { CacheModule, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MongoConfigDatabase } from './infrastructure/database/configs/databases/mongo-config.database';
-import { UsersModule } from './infrastructure/ioc/users.module';
+import { Module } from '@nestjs/common';
+// import { ConfigModule } from '@nestjs/config';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { MongoConfigDatabase } from './infrastructure/database/configs/databases/mongo-config.database';
+// import { UsersModule } from './infrastructure/ioc/users.module';
 // import { PostsModule } from './infrastructure/ioc/posts.module';
 // import { BlogsModule } from './infrastructure/ioc/blogs.module';
 // import { CommentsModule } from './infrastructure/ioc/comments.module';
-// import { TestController } from './presentation/controllers/test.controller';
-import { CacheService } from './infrastructure/cache';
+import { TestController } from './presentation/controllers/test.controller';
+// import { CacheService } from './infrastructure/cache';
 // import { APP_FILTER } from '@nestjs/core';
 // import { HttpExceptionFilter } from './infrastructure/rest/http-exception.filter';
 // import { AuthModule } from './infrastructure/ioc/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRootAsync({
-      useClass: MongoConfigDatabase,
-    }),
-    CacheModule.registerAsync({
-      useClass: CacheService,
-    }),
-    UsersModule,
+    // ConfigModule.forRoot({ isGlobal: true }),
+    // MongooseModule.forRootAsync({
+    //   useClass: MongoConfigDatabase,
+    // }),
+    // CacheModule.registerAsync({
+    //   useClass: CacheService,
+    // }),
+    // UsersModule,
     // PostsModule,
     // BlogsModule,
     // CommentsModule,
     // AuthModule,
   ],
-  controllers: [],
+  controllers: [TestController],
   // providers: [
   //   {
   //     provide: APP_FILTER,
