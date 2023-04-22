357b424d7e939ebf45adae68a3edf4139dce92ac
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-04-22 21:13:32 +0300

    feature: start HW 16

--- src/presentation/requests/sa/blogs/update-param-banned-by-blog.request.ts
+++ src/presentation/requests/sa/blogs/update-param-banned-by-blog.request.ts
@@ -1,9 +1,7 @@
 import { IsNotEmpty, IsString } from 'class-validator';
-import { ValidateBlogById } from '../../../../infrastructure/validators/validateBlogById.validator';
 
 export class UpdateParamBannedByBlogRequest {
   @IsString()
   @IsNotEmpty()
-  @ValidateBlogById()
   id: string;
 }
