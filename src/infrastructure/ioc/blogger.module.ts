b5ed3641f22df3ca8ca4df61fe7a8291590dead7
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-04-23 11:24:45 +0300

    feature: start HW 16

--- src/infrastructure/ioc/blogger.module.ts
+++ src/infrastructure/ioc/blogger.module.ts
@@ -15,6 +15,11 @@ import { CommentsModule } from './comments.module';
 import { BannedUserByBloggerAction } from '../../application/actions/blogger/users/banned-user-by-blogger.action';
 import { SaUsersModule } from './sa/users/sa-users.module';
 import { GetBannedUserAction } from '../../application/actions/blogger/users/get-banned-user.action';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../../domain/users/entities/user.entity';
+import { MongoCollections } from '../database/mongo.collections';
+import { Blog, BlogSchema } from '../../domain/blogs/entities/blog.entity';
+import { Comment, CommentSchema } from '../../domain/comments/entities/comment.entity';
 
 @Module({
   imports: [
@@ -26,6 +31,23 @@ import { GetBannedUserAction } from '../../application/actions/blogger/users/get
     }),
     CommentsModule,
     SaUsersModule,
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+        collection: MongoCollections.USERS,
+      },
+      {
+        name: Blog.name,
+        schema: BlogSchema,
+        collection: MongoCollections.BLOGS,
+      },
+      {
+        name: Comment.name,
+        schema: CommentSchema,
+        collection: MongoCollections.COMMENTS,
+      },
+    ]),
   ],
   controllers: [BloggerController],
   providers: [
