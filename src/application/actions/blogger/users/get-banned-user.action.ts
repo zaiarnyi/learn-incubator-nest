b2b261b250fb730d7b793c54a61f9d6266e77a30
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-04-23 10:56:01 +0300

    feature: start HW 16

--- src/application/actions/blogger/users/get-banned-user.action.ts
+++ src/application/actions/blogger/users/get-banned-user.action.ts
@@ -1,12 +1,23 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { GetUserBannedByBlogResponse } from '../../../../presentation/responses/blogger/get-user-banned-by-blog.response';
 import { QueryGetBannedUsersDto } from '../../../../domain/blogger/dto/query-get-banned-users.dto';
 import { QueryUserBannedRepository } from '../../../../infrastructure/database/repositories/sa/users/query-user-banned.repository';
 import { plainToClass } from 'class-transformer';
+import { QueryBlogsRepository } from '../../../../infrastructure/database/repositories/blogs/query-blogs.repository';
 
 @Injectable()
 export class GetBannedUserAction {
-  constructor(@Inject(QueryUserBannedRepository) private readonly userBannedRepository: QueryUserBannedRepository) {}
+  constructor(
+    @Inject(QueryUserBannedRepository) private readonly userBannedRepository: QueryUserBannedRepository,
+    @Inject(QueryBlogsRepository) private readonly blogRepository: QueryBlogsRepository,
+  ) {}
+
+  private async validateBlogId(blogId: string) {
+    const blog = await this.blogRepository.getBlogById(blogId);
+    if (!blog) {
+      throw new NotFoundException();
+    }
+  }
 
   public async execute(blogId: string, query: QueryGetBannedUsersDto): Promise<GetUserBannedByBlogResponse | any> {
     const totalCount = await this.userBannedRepository.getCountByBlog(query.searchLoginTerm, blogId);
