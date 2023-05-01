2bdf00946b56ecf53758c965e3b1a21b929682d6
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-05-01 18:57:55 +0300

    feature: start HW 17

--- src/application/actions/sa/users/delete-user.action.ts
+++ src/application/actions/sa/users/delete-user.action.ts
@@ -1,12 +1,20 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { UserMainRepository } from '../../../../infrastructure/database/repositories/users/main.repository';
+import { UserQueryRepository } from '../../../../infrastructure/database/repositories/users/query.repository';
 
 @Injectable()
 export class DeleteUserAction {
   logger = new Logger(DeleteUserAction.name);
-  constructor(private readonly repository: UserMainRepository) {}
+  constructor(
+    private readonly repository: UserMainRepository,
+    @Inject(UserQueryRepository) private readonly queryRepository: UserQueryRepository,
+  ) {}
 
   async execute(id: number): Promise<void> {
+    const user = await this.queryRepository.getUserById(id);
+    if (!user) {
+      throw new NotFoundException();
+    }
     await this.repository
       .deleteUserById(id)
       .then((res) => {
