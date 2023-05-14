33d159b4cfeee17ddb158fc434e332931e56f1d8
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-05-14 15:15:21 +0300

    feature: start HW 22

--- src/application/actions/blogs/getBlogById.action.ts
+++ src/application/actions/blogs/getBlogById.action.ts
@@ -15,7 +15,8 @@ export class GetBlogByIdAction {
       this.logger.error(`I can't find the blog. ID: ${id}`);
       throw new NotFoundException();
     });
-    if (!findBlog) {
+
+    if (!findBlog || findBlog.isBanned) {
       throw new NotFoundException();
     }
     return plainToClass(CreateBlogResponse, {
