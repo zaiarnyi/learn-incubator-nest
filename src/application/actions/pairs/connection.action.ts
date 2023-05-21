d5265ab604a9f634c1506a452f7e7fd10720cd4f
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-05-21 20:38:37 +0300

    feature: added endpoint for test rules

--- src/application/actions/pairs/connection.action.ts
+++ src/application/actions/pairs/connection.action.ts
@@ -29,8 +29,7 @@ export class ConnectionPairAction {
     if (!pair) return null;
 
     if (pair.firstPlayer.id === user.id) {
-      console.log(pair.status, 'pair');
-      return pair;
+      throw new ForbiddenException();
     }
 
     pair.secondPlayer = user;
