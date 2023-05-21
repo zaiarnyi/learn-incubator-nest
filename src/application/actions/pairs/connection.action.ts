a216aeec89a826a408df821a21e7cd3fb6334a3d
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-05-21 16:05:37 +0300

    feature: added endpoint for test rules

--- src/application/actions/pairs/connection.action.ts
+++ src/application/actions/pairs/connection.action.ts
@@ -29,9 +29,9 @@ export class ConnectionPairAction {
       this.repository.getActiveGameByUserId(user),
     ]);
 
-    if (hasActiveGame) {
-      throw new ForbiddenException();
-    }
+    // if (hasActiveGame) {
+    //   throw new ForbiddenException();
+    // }
     if (!pair) return null;
 
     if (pair?.firstPlayer?.id === user.id || pair?.secondPlayer?.id === user.id) {
