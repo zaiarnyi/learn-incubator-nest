cc6a6ebad12e4e636e8ff47cf3f6c5aa423eb09b
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-06-06 21:52:48 +0300

    feature: added logic for save files to aws

--- src/infrastructure/database/repositories/users/main.repository.ts
+++ src/infrastructure/database/repositories/users/main.repository.ts
@@ -34,20 +34,20 @@ export class UserMainRepository {
   async deleteAllData() {
     await Promise.all([
       this.dataSource.query(
-        `TRUNCATE TABLE users, activate_emails_code,pair_results, user_security, users_banned,invalid_tokens, comments, blogs, posts, comment_likes, post_likes,pairs_questions_quiz, quiz, pairs, pair_answers  CASCADE;`,
+        `TRUNCATE TABLE users, activate_emails_code,pair_results, user_security, users_banned,invalid_tokens, comments, blog_images, post_images, blogs, posts, comment_likes, post_likes,pairs_questions_quiz, quiz, pairs, pair_answers  CASCADE;`,
       ),
       this.dataSource.query(
-        `TRUNCATE TABLE users, activate_emails_code,pair_results, user_security, users_banned, invalid_tokens, comments, blogs, posts, comment_likes, post_likes,pairs_questions_quiz, quiz, pairs, pair_answers RESTART IDENTITY;`,
+        `TRUNCATE TABLE users, activate_emails_code,pair_results, user_security, users_banned, invalid_tokens, comments, blog_images, post_images, blogs, posts, comment_likes, post_likes,pairs_questions_quiz, quiz, pairs, pair_answers RESTART IDENTITY;`,
       ),
     ]);
   }
   async deletePairAndUser() {
     await Promise.all([
       this.dataSource.query(
-        `TRUNCATE TABLE users, invalid_tokens,users_banned, user_security, pairs_questions_quiz, activate_emails_code, blogs,posts, comments, comment_likes, post_likes, pair_results,pairs, pair_answers CASCADE;`,
+        `TRUNCATE TABLE users, invalid_tokens,users_banned, user_security, pairs_questions_quiz, activate_emails_code, blog_images, post_images, blogs,posts, comments, comment_likes, post_likes, pair_results,pairs, pair_answers CASCADE;`,
       ),
       this.dataSource.query(
-        `TRUNCATE TABLE users, invalid_tokens, pair_results, users_banned, user_security, pairs_questions_quiz, activate_emails_code, blogs,posts, comments, comment_likes, post_likes, pairs, pair_answers RESTART IDENTITY;`,
+        `TRUNCATE TABLE users, invalid_tokens, pair_results, users_banned, user_security, pairs_questions_quiz, activate_emails_code, blog_images, post_images, blogs,posts, comments, comment_likes, post_likes, pairs, pair_answers RESTART IDENTITY;`,
       ),
     ]);
   }
