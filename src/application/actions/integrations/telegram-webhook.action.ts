797877aa3225a74f3ecda00dd46dfdb657b61003
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-06-12 18:36:09 +0300

    feature: added swagger

--- src/application/actions/integrations/telegram-webhook.action.ts
+++ src/application/actions/integrations/telegram-webhook.action.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { UserMainRepository } from '../../../infrastructure/database/repositories/users/main.repository';
 
 @Injectable()
 export class TelegramWebhookAction {
+  private readonly logger = new Logger(TelegramWebhookAction.name);
   constructor(private readonly userRepository: UserMainRepository) {}
   public async execute(body: Record<string, any>) {
     console.log(body, 'body');
@@ -10,8 +11,9 @@ export class TelegramWebhookAction {
       if (!body.message.text.includes('/start')) return null;
 
       const uuid = body.message.text.split(' ')[1].replace(/code=/, '');
-
-      await this.userRepository.setTelegramIdUsers(uuid, body.message.from.id);
+      console.log(uuid, 'uuid');
+      const update = await this.userRepository.setTelegramIdUsers(uuid, body.message.from.id);
+      console.log(update, 'update');
     } catch (e) {}
   }
 }
