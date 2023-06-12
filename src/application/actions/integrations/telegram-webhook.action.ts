60d652bdfea16659271fca75ba39ce6eea66386c
Denis Zaiarnyi <fl.zaiarnyi@gmail.com>
2023-06-12 18:59:32 +0300

    feature: added swagger

--- src/application/actions/integrations/telegram-webhook.action.ts
+++ src/application/actions/integrations/telegram-webhook.action.ts
@@ -6,17 +6,11 @@ export class TelegramWebhookAction {
   private readonly logger = new Logger(TelegramWebhookAction.name);
   constructor(private readonly userRepository: UserMainRepository) {}
   public async execute(body: Record<string, any>) {
-    console.log(body, 'body');
     try {
-      if (!body.message.text.includes('/start')) return null;
+      if (!body?.message?.text?.includes('/start')) return null;
 
       const uuid = body.message.text.split(' ')[1].replace(/code=/, '');
-      console.log(uuid, 'uuid');
-      const update = await this.userRepository.setTelegramIdUsers(
-        uuid,
-        body?.message?.from?.id ?? body?.message?.chat?.id,
-      );
-      console.log(update, 'update');
+      await this.userRepository.setTelegramIdUsers(uuid, body?.message?.from?.id ?? body?.message?.chat?.id);
     } catch (e) {
       this.logger.error(`Error telegram webhook. Message: ${e.message}`);
     }
--- src/domain/posts/listeners/created-post.listener.ts
+++ src/domain/posts/listeners/created-post.listener.ts
@@ -11,7 +11,6 @@ export class CreatedPostListener {
   constructor(private readonly queryBlogRepository: QueryBlogsRepository, private readonly httpService: HttpService) {}
 
   private async sendMessage(telegramId: number, message: string): Promise<void> {
-    console.log(telegramId, message);
     const link = `https://api.telegram.org/bot6203822227:AAFW725L9J4S55TfW0_kxc3RSKeyJawY3ow/sendMessage`;
 
     const response = await firstValueFrom(this.httpService.post(link, { chat_id: telegramId, text: message }));
@@ -22,7 +21,7 @@ export class CreatedPostListener {
     const message = `New post published for blog "${event.blog.name}`;
 
     const subscriptions = await this.queryBlogRepository.subscriptionForBlog(event.blog.id);
-    console.log(subscriptions, 'subscriptions');
+
     for (const subscription of subscriptions) {
       if (!subscription.user.telegramId) continue;
       await this.sendMessage(subscription.user.telegramId, message).catch((e) => {
